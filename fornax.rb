f79fff08f6cb9acfd5cb3d50d89a23885487003f
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2017-06-05 00:22:30 +0200

    Update fornax.rb

--- fornax.rb
+++ fornax.rb
@@ -10,7 +10,7 @@ class Fornax < Formula
 
   def install
     libexec.install Dir["./*"]
-    ("forge").write <<-EOS.undent
+    (bin/"fornax").write <<-EOS.undent
       #!/bin/sh
       mono #{libexec}/Fornax.exe "$@"
     EOS
